7a76b16b5a3df7b9fcebad5999b1b49e820bc29a
Jenna Rowan <jennagirl001@gmail.com>
2023-10-21 18:39:27 -0400

    New code

---
+++ server/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import openai
+
+# app instance
+app = Flask(__name__)
+CORS(app)
+
+# /api/home
+@app.route("/api/home", methods=['GET'])
+def return_home():
+    return jsonify({
+        'message': "The best group at Technica is:",
+        'people': ['Fariha', 'Irene', 'Isyhara', 'Jenna']
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
